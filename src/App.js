6ed15f4a30034a67e0abe34ca31e3c43467e8749
Peter Lind <petl@kea.dk>
2021-05-03 10:56:59 +0200

    first commit with components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SimpleNav></SimpleNav>
+      <ProductList>
+        
+      </ProductList>  
+      <Basket>
+      </Basket>
     </div>
   );
+}
+
+function SimpleNav(){
+  return (
+    <nav className="SimpleNav">
+      <h1>Simple navigation</h1>
+    </nav>
+  );
+}
+
+function ProductList() {
+  return (
+    <div className="ProductList">
+        <Product></Product>  
+        <Product></Product>  
+        <Product></Product>  
+        <Product></Product>  
+        <Product></Product>  
+        <Product></Product>  
+    </div>
+  )
+}
+
+function Product() {
+  return (
+    <p>product details</p>
+  )
+}
+
+function Basket() {
+  return (
+    <div className="Basket">
+      <MyBasket></MyBasket>
+      <CheckoutForm></CheckoutForm>
+    </div>
+  )
+}
+
+function MyBasket() {
+  return(
+    <p>Displays #items and price</p>
+  )
+}
+
+function CheckoutForm() {
+  return (
+    <p>Some checkout form</p>
+  )
 }
 
 export default App;
